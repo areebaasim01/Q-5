1d105d1c2a9d8d875816a681e70fca12fe270e6b
areebaasim01 <161856617+areebaasim01@users.noreply.github.com>
2024-04-11 22:29:48 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+// 5.	Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person.
+//  Then compose your message and store it in a new variable called message. 
+// Print your message.
+Object.defineProperty(exports, "__esModule", { value: true });
+let famous_person = "Michael Corleone";
+let message = "Keep your friends close, but your enemies closer.";
+console.log(`${famous_person} Once Said, "${message}" `);
---
+++ index.ts
@@ -0,0 +1,12 @@
+// 5.	Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person.
+//  Then compose your message and store it in a new variable called message. 
+// Print your message.
+
+
+let famous_person: string = "Michael Corleone";
+let message: string = "Keep your friends close, but your enemies closer."
+
+
+console.log(`${famous_person} Once Said, "${message}" `);
+
+
